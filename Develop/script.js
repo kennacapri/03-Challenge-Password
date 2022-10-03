9444e7bdd3c043f609c227344444f3891d3b1889
kennacapri <kennac28@gmail.com>
2022-10-03 13:03:55 -0600

    added possible characters to password (numbers, symbols, upper or lowercase)

--- Develop/script.js
+++ Develop/script.js
@@ -6,13 +6,28 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
-  var userInput = window.prompt("How long doe you want your password?")
+  var userInput = window.prompt("How long do you want your password?")
 
   var passwordLength = parseInt(userInput)
 
- if (isNaN)(passwordLength)) {
+ if (isNaN(passwordLength)) {
   window.alert("That isn't a number")
   return
+
+}
+// || means "or"
+if (passwordLength < 8 || passwordLength > 128) {
+  window.alert("Password length has to be between 8-128")
+  return
+}
+
+var userAddsNumbers = window.confirm("Do you want to add numbers to your password")
+var userAddsSymbols = window.confirm("Do you want to add symbols to your password")
+var userAddsLowercase = window.confirm("Do you want to add lowercase letters to your password")
+var userAddsUppercase = window.confirm("Do you want to add uppercase letters to your password")
+
+
+
 
 }
 
