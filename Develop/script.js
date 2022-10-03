cc23cb42f9f3b50c164072b0d0bb8c8ce48c9db6
kennacapri <kennac28@gmail.com>
2022-10-03 12:46:43 -0600

    added password length info to javascript

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,19 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
+  var userInput = window.prompt("How long doe you want your password?")
+
+  var passwordLength = parseInt(userInput)
+
+ if (isNaN)(passwordLength)) {
+  window.alert("That isn't a number")
+  return
+
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
