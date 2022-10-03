2ea13e70a90a22a68da4340f1a1cf3ca9dda0c78
kennacapri <kennac28@gmail.com>
2022-10-03 13:22:05 -0600

    added if statements to possible password

--- Develop/script.js
+++ Develop/script.js
@@ -28,8 +28,23 @@ var userAddsUppercase = window.confirm("Do you want to add uppercase letters to 
 
 var possibleNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var possibleSymbols = ["!", "@", "#", "%", "&", "*", "?", "/"]
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var possibleLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var possibleUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+var optionsPassword = []
+
+if (userAddsNumbers --- true) [
+  optionsPassword.push(possibleNumbers)
+]
+if (userAddsSymbols --- true) [
+  optionsPassword.push(possibleSymbols)
+]
+if (userAddsLowercase --- true) [
+  optionsPassword.push(possibleLowercase)
+]
+if (userAddsUppercase --- true) [
+  optionsPassword.push(possibleUppercase)
+]
 
 }
 
